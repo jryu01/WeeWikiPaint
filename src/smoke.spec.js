6f98e36ec40c3656933335b7e423c381d638a8d3
Jaehwan Ryu <time0121@gmail.com>
2015-10-05 13:35:11 -0400

    Update smoke test

--- src/smoke.spec.js
+++ src/smoke.spec.js
@@ -3,11 +3,27 @@
 
 var expect = require('chai').expect,
     http = require('http'),
+    fs = require('fs'),
     childProcess = require('child_process'), 
     child;
 
+var PORT_NUM = 5000, 
+    HOST = 'http://localhost:' + PORT_NUM;
+
+
+var parseProcfile = function () {
+  var procfile = fs.readFileSync('Procfile', { encoding: 'utf8' }),
+      parsedCommand = procfile.trim()
+        .replace('$PORT', 5000).split(' ').slice(1);
+  return {
+    command: parsedCommand.shift(),
+    args: parsedCommand
+  };
+};
+
 var runServer = function (callback) {
-  child = childProcess.spawn('node', ['src/server/weewikipaint', '8080']);
+  var commandLine = parseProcfile();
+  child = childProcess.spawn(commandLine.command, commandLine.args);
   child.stdout.setEncoding('utf8');
   child.stdout.on('data', function (chunk) {
     if (chunk.trim() === 'Server started') {
@@ -43,7 +59,7 @@ describe('Smoke test', function () {
   });
 
   it('should get the hompage', function (done) {
-    httpGet('http://localhost:8080', function (res, data) {
+    httpGet(HOST, function (res, data) {
       var foundHomePage = data.indexOf('weewikipaint home page') !== -1;
       expect(foundHomePage).to.be.ok;
       done();
@@ -51,7 +67,7 @@ describe('Smoke test', function () {
   });
 
   it('should get the 404 page', function (done) {
-    httpGet('http://localhost:8080/nonexistant.html', function (res, data) {
+    httpGet(HOST + '/nonexistant.html', function (res, data) {
       var foundPage = data.indexOf('weewikipaint 404 page') !== -1;
       expect(foundPage).to.be.ok;
       done();
