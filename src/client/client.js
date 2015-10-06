39d0849620531030ca1e37e2955c32469ae184e5
Jaehwan Ryu <time0121@gmail.com>
2015-10-05 23:15:26 -0400

    Made basic dom testing working

--- src/client/client.js
+++ src/client/client.js
@@ -1,7 +1,11 @@
+/*globals wwp*/
+
+wwp = {};
+
 (function () {
   'use strict';
 
-  window.onload = function () {
+  wwp.createElement = function () {
     var element = document.createElement('div');
     element.setAttribute('id', 'tdjs');
     element.setAttribute('foo', 'bar');
--- src/client/client.spec.js
+++ src/client/client.spec.js
@@ -1,12 +1,13 @@
 'use strict';
 /*jshint expr: true*/
-/*globals dump, chai*/
+/*globals dump, wwp:true*/
 
 var expect = chai.expect;
 
 describe('Nothing', function () {
 
   it('should run', function () {
+    wwp.createElement();
     var extractedDiv = document.getElementById('tdjs');
     expect(extractedDiv.getAttribute('foo')).to.equal('bar');
   });
