a97e0fef0f40fd8ff5a7887c256dcbd358f9d74f
Jaehwan Ryu <time0121@gmail.com>
2015-11-10 19:47:35 -0500

    Cleanup test code

--- src/client/client.js
+++ src/client/client.js
@@ -5,38 +5,37 @@ wwp = {};
 (function () {
   'use strict';
 
-  // var raphael = Raphael;
   var paper;
 
   wwp.initializeDrawingArea = function (drawingAreaElement) {
-    var $drawingArea = $(drawingAreaElement);
-    var prevX;
-    var prevY;
-    var isDragging = false;
     paper = new Raphael(drawingAreaElement);
-
-    $drawingArea.mousemove(function (event) {
-      var x = event.offsetX;
-      var y = event.offsetY;
-      if (prevX && isDragging) {
-        wwp.drawLine(prevX, prevY, x, y);
-      }
-      prevX = x;
-      prevY = y;
-    });
-
-    $drawingArea.mouseenter(function (event) {
-      prevX = event.offsetX;
-      prevY = event.offsetY;
-    });
-
-    $drawingArea.mousedown(function (event) {
-      isDragging = true;
-    });
-
-    $drawingArea.mouseup(function (event) {
-      isDragging = false;
-    });
+    // var $drawingArea = $(drawingAreaElement);
+    // var prevX;
+    // var prevY;
+    // var isDragging = false;
+    //
+    // $drawingArea.mousemove(function (event) {
+    //   var x = event.offsetX;
+    //   var y = event.offsetY;
+    //   if (prevX && isDragging) {
+    //     wwp.drawLine(prevX, prevY, x, y);
+    //   }
+    //   prevX = x;
+    //   prevY = y;
+    // });
+    //
+    // $drawingArea.mouseenter(function (event) {
+    //   prevX = event.offsetX;
+    //   prevY = event.offsetY;
+    // });
+    //
+    // $drawingArea.mousedown(function (event) {
+    //   isDragging = true;
+    // });
+    //
+    // $drawingArea.mouseup(function (event) {
+    //   isDragging = false;
+    // });
 
     return paper;
   };
--- src/client/client.spec.js
+++ src/client/client.spec.js
@@ -7,46 +7,41 @@ var expect = chai.expect;
 describe('Drawing area', function () {
 
   var $drawingDiv;
+  var paper;
+
+  var drawingElements = function (paper) {
+    var elements = [];
+		paper.forEach(function (element) {
+			elements.push(element);
+		});
+    return elements;
+  };
+  var pathFor = function (drawingElement) {
+    var box = drawingElement.getBBox();
+    return 'M' + box.x + ',' + box.y + 'L' + box.x2 + ',' + box.y2;
+  };
+
+  beforeEach(function () {
+    // create div that's assumed to be in our home page
+    $drawingDiv = $('<div style="height: 200px; width: 400px;"></div>');
+    $(document.body).append($drawingDiv);
+    //initialize paper
+    paper = wwp.initializeDrawingArea($drawingDiv[0]);
+  });
 
   afterEach(function () {
     $drawingDiv.remove();
   });
 
-  it('should be initialized in predefined div', function () {
-    // create div that's assumed to be in our home page
-    $drawingDiv = $('<div></div>');
-    $(document.body).append($drawingDiv);
-
-    // initialize it
-    wwp.initializeDrawingArea($drawingDiv[0]);
-
-    // verify it was initialized correctly
-    var tagName = $drawingDiv.children()[0].tagName;
-    expect(tagName).to.equal('svg');
-  });
-
   it('should have same dimensions as its enclosing div', function () {
-    $drawingDiv = $('<div style="height: 200px; width: 400px;"></div>');
-    $(document.body).append($drawingDiv);
-
-    var paper = wwp.initializeDrawingArea($drawingDiv[0]);
     expect(paper.width).to.equal(400);
     expect(paper.height).to.equal(200);
   });
 
   it('should draw a line', function () {
-    $drawingDiv = $('<div style="height: 200px; width: 400px;">hi</div>');
-    $(document.body).append($drawingDiv);
-
-    var paper = wwp.initializeDrawingArea($drawingDiv);
     wwp.drawLine(20, 40, 90, 90);
-
-    var elements = [];
-		paper.forEach(function (element) {
-			elements.push(element);
-		});
-    var path = elements[0].node.attributes.d.value;
-    expect(path).to.equal('M20,40L90,90');
+    var elements = drawingElements(paper);
+    expect(pathFor(elements[0])).to.equal('M20,40L90,90');
   });
 
 });
